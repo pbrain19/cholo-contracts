1f92605512b4976d9f89a026e17c1535d071413d
Paul Garcia <pbrain19@gmail.com>
2025-03-06 12:59:29 -0500

    removed deployments

--- ignition/modules/CholoDrome.ts
+++ ignition/modules/CholoDrome.ts
@@ -8,7 +8,6 @@ const CholoDromeModule = buildModule("CholoDromeModule", (m) => {
   const uniswapSwapRouterAddress = "0x2626664c2603336E57B271c5C0b26F421741e481";
   const rewardToken = "0x940181a94A35A4569E4529A3CDfB74e38FD98631";
   const usdtAddress = "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2";
-  const usdcAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
   const weth = "0x4200000000000000000000000000000000000006";
   const deployerAddress = m.getAccount(0);
 
@@ -18,7 +17,6 @@ const CholoDromeModule = buildModule("CholoDromeModule", (m) => {
     rewardToken, // _rewardToken
     usdtAddress, // _rewardStable
     uniswapSwapRouterAddress, // _swapRouter
-    usdcAddress, // _usdc
     weth, // _weth
   ]);
 
